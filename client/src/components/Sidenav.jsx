7e884ef69b299c936992eca262e9afd31798dc4e
DragomirMollov <44067945+DragomirMollov@users.noreply.github.com>
2024-08-09 23:24:16 +0100

    change view sidebar

--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -50,13 +50,13 @@ export default function Sidenav() {
                     {/* <Link to="main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <AiOutlineHome size={20} />
                         </Link> */}
+                    <Link to="projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineProject size={20} />
+                    </Link>
                     {isAuthenticated ? 
                     (<Link to="work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <GrProjects size={20} />
                     </Link>):('')}
-                    <Link to="projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineProject size={20} />
-                    </Link>
                     {isAuthenticated ?
                         (<Link to="projects/create" className='rounded-full z-20 shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <BsPerson size={20} />
