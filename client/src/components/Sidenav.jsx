0046eeed1feb3cf5ec54e9b793ea23e5e9711c91
DragomirMollov <44067945+DragomirMollov@users.noreply.github.com>
2024-08-09 22:49:04 +0100

    conditional rendernig for Work component

--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -3,12 +3,14 @@ import { AiOutlineHome, AiOutlineMenu, AiOutlineProject, AiOutlineMail } from 'r
 import { BsPerson } from 'react-icons/bs';
 import { GrProjects } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../contexts/AuthContext';
 
 export default function Sidenav() {
     const [nav, setNav] = useState(false);
     const handleNAv = () => {
         setNav(!nav);
     };
+    const { isAuthenticated } = useAuthContext();
     return (
         <div>
             <AiOutlineMenu
@@ -30,38 +32,41 @@ export default function Sidenav() {
                             <AiOutlineProject size={20} />
                             <span className='pl-4'>Projects</span>
                         </Link>
-                        {/* <Link onClick={handleNAv} to="main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        {/* <Link onClick={handleNAv} to="maiprojects/createn" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                             <BsPerson size={20} />
-                            <span className='pl-4'>Resume</span>
+                            <span className='pl-4'>Create</span>
                         </Link> */}
-                        <Link onClick={handleNAv} to="contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        {/* <Link onClick={handleNAv} to="contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                             <AiOutlineMail size={20} />
                             <span className='pl-4'>Contact</span>
-                        </Link>
+                        </Link> */}
                     </div>
                 ) : (
-                 ''
+                    ''
                 )}
 
-                <div className='md:block hidden fixed top-[25%] z-10'>
-                    <div className='flex flex-col'>
-                        {/* <Link to="main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <div className='md:block hidden fixed top-[25%] z-10'>
+                <div className='flex flex-col'>
+                    {/* <Link to="main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <AiOutlineHome size={20} />
                         </Link> */}
-                        <Link to="work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <GrProjects size={20} />
-                        </Link>
-                        <Link to="projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                            <AiOutlineProject size={20} />
-                        </Link>
-                        <Link to="projects/create" className='rounded-full z-20 shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    {isAuthenticated ? 
+                    (<Link to="work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <GrProjects size={20} />
+                    </Link>):('')}
+                    <Link to="projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineProject size={20} />
+                    </Link>
+                    {isAuthenticated ?
+                        (<Link to="projects/create" className='rounded-full z-20 shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <BsPerson size={20} />
-                        </Link>
-                        <Link to="contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        </Link>) : ('')};
+
+                    {/* <Link to="contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <AiOutlineMail size={20} />
-                        </Link>
-                    </div>
+                        </Link> */}
                 </div>
+            </div>
         </div>
     );
 };
