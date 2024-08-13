7074b877eda9a44a5387d954a3476bcb851bb6e7
DragomirMollov <44067945+DragomirMollov@users.noreply.github.com>
2024-08-13 20:42:52 +0100

    add guards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,8 @@ import ProjectDetails from './components/project-details/ProjectDetails';
 import ProjectCreate from './components/project-create/ProjectCreate';
 import ProjectEdit from './components/project-edit/ProjectEdit';
 import Work from './components/work/Work';
+import RouteGuard from './components/common/RouteGuard';
+import PrivetGuard from './components/common/PrivateGuard';
 
 function App() {
   return (
@@ -22,13 +24,21 @@ function App() {
           <Routes>
             <Route path="/" element={<Sidenav />} />
             <Route path="/work" element={<Work />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/register" element={<RouteGuard><Register /></RouteGuard>} />
             <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/projects/:projectId/details" element={<ProjectDetails />} />
-            <Route path="/projects/create" element={<ProjectCreate />} />
+            <Route element={<PrivetGuard />}>
+                <Route path='/projects/create' element={<ProjectCreate />} />
+                <Route path="/projects/:projectId/details" element={<ProjectDetails />} />
+                <Route path="/logout" element={<Logout />} />
+            </Route>  
+            <Route path="/projects/create" element={<RouteGuard><ProjectCreate /></RouteGuard>} />
+            {/* <Route path="/projects/create" element={(
+                    <RouteGuard>
+                        <ProjectCreate />
+                    </RouteGuard>
+            )} /> */}
             <Route path="/projects/:projectId/edit" element={<ProjectEdit />} />
           </Routes>
         </main>
